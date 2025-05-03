d16788ffb0714daa13e3a32e0fd6307c96db8ac8
NgThanhTrung124 <ngtrungbo06@gmail.com>
2025-05-03 23:07:28 +0700

    Initial project

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let menuToggle = document.getElementById("menu-toggle");
+    let mobileNav = document.getElementById("mobile-nav");
+    let barsIcon = document.getElementById("bars-icon");
+    let closeIcon = document.getElementById("close-icon");
+
+    menuToggle.addEventListener("click", function(){
+        mobileNav.classList.toggle("active");
+    });
+    if(mobileNav.classList.contains("active")){
+        barsIcon.style.display = "none";
+        closeIcon.style.display = "block";
+    } else {
+        barsIcon.style.display = "block";
+        closeIcon.style.display = "none";
+    }
+});
